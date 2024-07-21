e488ad88b2faca3011a4d864d8c5aad2bb8587bd
Rahul Khanal <92104751+rahulkhanal@users.noreply.github.com>
2024-07-21 10:14:44 +0545

    project strucuture changed

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -3,16 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import foo.bar.WelcomeMessage;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
-		var WelcomeMessage = new WelcomeMessage();
-		System.out.println(WelcomeMessage.getWelcomeMessage());
 	}
 
 }
